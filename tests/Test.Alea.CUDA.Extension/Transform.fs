ec46ed602c689bfb1694de7eb3f682b2a98c5357
Xiang Zhang <soloman817@gmail.com>
2013-02-07 18:51:38 +0800

    Construct the solution

---
+++ tests/Test.Alea.CUDA.Extension/Transform.fs
@@ -0,0 +1,33 @@
+﻿module Test.Alea.CUDA.Extension.Transform
+
+open NUnit.Framework
+open Alea.CUDA
+open Alea.CUDA.Extension
+
+let rng = System.Random()
+
+let gpuMap transform =
+    let worker = Engine.workers.DefaultWorker
+    let mapper = worker.LoadPModule(Transform.map transform).Invoke
+    fun input -> mapper.Map(input)
+
+[<Test>]
+let ``map log<float>``() =
+    let gpuMapLog = gpuMap <@ log @>
+    let n = 1 <<< 22
+    let input = Array.init n (fun _ -> rng.NextDouble())
+    let hOutput = input |> Array.map log
+    let dOutput = input |> gpuMapLog
+    (hOutput, dOutput) ||> Array.iter2 (fun h d -> Assert.That(d, Is.EqualTo(h).Within(1e-10)))
+
+[<Test>]
+let ``map log<float32>``() =
+    let gpuMapLog = gpuMap <@ log @>
+    let n = 1 <<< 22
+    let input = Array.init n (fun _ -> rng.NextDouble() |> float32)
+    let hOutput = input |> Array.map log
+    let dOutput = input |> gpuMapLog
+    (hOutput, dOutput) ||> Array.iter2 (fun h d -> Assert.That(d, Is.EqualTo(h).Within(1e-6)))
+
+
+
