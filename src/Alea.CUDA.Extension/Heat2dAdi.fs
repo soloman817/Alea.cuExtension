e8da461a94302e5f0936211799094c3a7e019289
Daniel Egloff <daniel.egloff@quantalea.net>
2013-03-10 13:26:48 +0100

    Fixed Heat2dAdi to match expected results

--- src/Alea.CUDA.Extension/Heat2dAdi.fs
+++ src/Alea.CUDA.Extension/Heat2dAdi.fs
@@ -10,132 +10,65 @@ open Util
 
 let [<ReflectedDefinition>] pi = System.Math.PI
 
+/// Solves ny-1 systems of dimension nx+1 in the x-coordinate direction 
 [<ReflectedDefinition>]
 let xSweep (boundary:float -> float -> float -> float) (sourceFunction:float -> float -> float -> float)
            nx ny (x:DevicePtr<float>) (y:DevicePtr<float>) (Cx:float) (Cy:float) (dt:float) (t0:float) (t1:float) (u0:DevicePtr<float>) (u1:DevicePtr<float>) =
     let shared = __extern_shared__()
     let h = shared.Reinterpret<float>()
-    //let h = shared.Reinterpret<'T>()
     let d = h + (nx+1)
     let l = d + (nx+1)
     let u = l + (nx+1)
 
-    let mutable xj = 0.0
-    let mutable yi = 0.0
+    let mutable xi = 0.0
+    let mutable yj = 0.0
 
     let mstride = ny+1
 
-    let mutable i = blockIdx.x
-    while i <= ny do  
-        yi <- y.[i]
+    let mutable j = blockIdx.x
+    while j <= ny do  
+        yj <- y.[j]
 
-        if i = 0 || i = ny then
+        if j = 0 || j = ny then
 
-            let mutable j = threadIdx.x
-            while j <= nx do  
-                xj <- x.[j]
-                u1.[i*mstride+j] <- boundary t1 xj yi 
-                j <- j + blockDim.x
+            let mutable i = threadIdx.x
+            while i <= nx do  
+                xi <- x.[i]
+                u1.[i*mstride+j] <- boundary t1 xi yj 
+                i <- i + blockDim.x
 
             __syncthreads()
 
         else
 
-            let mutable j = threadIdx.x
-            while j <= nx do
-                xj <- x.[j]
+            let mutable i = threadIdx.x
+            while i <= nx do
+                xi <- x.[i]
 
-                if j = 0 then
-                    d.[j] <- 1.0
-                    u.[j] <- 0.0
-                    h.[j] <- boundary t1 xj yi
-                else if j = nx then
-                    l.[j] <- 0.0
-                    d.[j] <- 1.0
-                    h.[j] <- boundary t1 xj yi
+                if i = 0 then
+                    d.[i] <- 1.0
+                    u.[i] <- 0.0
+                    h.[i] <- boundary t1 xi yj
+                else if i = nx then
+                    l.[i] <- 0.0
+                    d.[i] <- 1.0
+                    h.[i] <- boundary t1 xi yj
                 else
-                    l.[j] <- -Cx
-                    d.[j] <- 2.0 + 2.0*Cx
-                    u.[j] <- -Cx
-                    h.[j] <- 2.0*u0.[i*mstride+j] +
-                             Cy*(u0.[(i-1)*mstride+j] - 2.0*u0.[i*mstride+j] + u0.[(i+1)*mstride+j]) +
-                             dt*(sourceFunction t1 xj yi)
+                    l.[i] <- -Cx
+                    d.[i] <- 2.0 + 2.0*Cx
+                    u.[i] <- -Cx
+                    h.[i] <- 2.0*u0.[i*mstride+j] +
+                             Cy*(u0.[i*mstride+(j-1)] - 2.0*u0.[i*mstride+j] + u0.[i*mstride+(j+1)]) +
+                             dt*(sourceFunction t1 xi yj)
 
-                j <- j + blockDim.x
+                i <- i + blockDim.x
 
             __syncthreads()
 
             triDiagPcrSingleBlock (nx+1) l d u h
 
-            j <- threadIdx.x
-            while j <= nx do  
-                u1.[i*mstride+j] <- h.[j]
-                j <- j + blockDim.x
-
-            __syncthreads()
-
-        i <- i + gridDim.x
-
-[<ReflectedDefinition>]
-let ySweep (boundary:float -> float -> float -> float) (sourceFunction:float -> float -> float -> float)
-           nx ny (x:DevicePtr<float>) (y:DevicePtr<float>) (Cx:float) (Cy:float) (dt:float) (t0:float) (t1:float) (u0:DevicePtr<float>) (u1:DevicePtr<float>) =
-    let shared = __extern_shared__()
-    //let h = shared.Reinterpret<'T>()
-    let h = shared.Reinterpret<float>()
-    let d = h + (nx+1)
-    let l = d + (nx+1)
-    let u = l + (nx+1)
-
-    let mutable xj = 0.0
-    let mutable yi = 0.0
-
-    let mstride = ny+1
-
-    let mutable j = blockIdx.x
-    while j <= nx do
-
-        xj <- x.[j]
-
-        if j = 0 || j = nx then
-
-            let mutable i = threadIdx.x
-            while i <= ny do
-                yi <- y.[i]
-                u1.[i*mstride+j] <- boundary t1 xj yi
-                i <- i + blockDim.x
-
-            __syncthreads()
-        
-        else
-
-            let mutable i = threadIdx.x
-            while i <= ny do  
-                yi <- y.[i]
-
-                if i = 0 then
-                    d.[i] <- 1.0
-                    u.[i] <- 0.0
-                    h.[i] <- boundary t1 xj yi
-                else if i = ny then
-                    l.[i] <- 0.0
-                    d.[i] <- 1.0
-                    h.[i] <- boundary t1 xj yi
-                else
-                    l.[i] <- -Cy
-                    d.[i] <- 2.0 + 2.0*Cy
-                    u.[i] <- -Cy
-                    h.[i] <- 2.0*u0.[i*mstride+j] +
-                             Cx*(u0.[i*mstride+j-1] - 2.0*u0.[i*mstride+j] + u0.[i*mstride+j+1]) +
-                             dt*(sourceFunction t1 xj yi)
-
-                i <- i + blockDim.x
-
-            __syncthreads()
-
-            triDiagPcrSingleBlock (ny+1) l d u h
-
             i <- threadIdx.x
-            while i <= ny do 
+            while i <= nx do  
                 u1.[i*mstride+j] <- h.[i]
                 i <- i + blockDim.x
 
@@ -143,12 +76,81 @@ let ySweep (boundary:float -> float -> float -> float) (sourceFunction:float -> 
 
         j <- j + gridDim.x
 
+/// Solves nx-1 systems of dimension ny+1 in the y-coordinate direction 
+[<ReflectedDefinition>]
+let ySweep (boundary:float -> float -> float -> float) (sourceFunction:float -> float -> float -> float)
+           nx ny (x:DevicePtr<float>) (y:DevicePtr<float>) (Cx:float) (Cy:float) (dt:float) (t0:float) (t1:float) (u0:DevicePtr<float>) (u1:DevicePtr<float>) =
+    let shared = __extern_shared__()
+    let h = shared.Reinterpret<float>()
+    let d = h + (nx+1)
+    let l = d + (nx+1)
+    let u = l + (nx+1)
+
+    let mutable xi = 0.0
+    let mutable yj = 0.0
+
+    let mstride = ny+1
+
+    let mutable i = blockIdx.x
+    while i <= nx do
+
+        xi <- x.[i]
+
+        if i = 0 || i = nx then
+
+            let mutable j = threadIdx.x
+            while j <= ny do
+                yj <- y.[j]
+                u1.[i*mstride+j] <- boundary t1 xi yj
+                j <- j + blockDim.x
+
+            __syncthreads()
+        
+        else
+
+            let mutable j = threadIdx.x
+            while j <= ny do  
+                yj <- y.[j]
+
+                if j = 0 then
+                    d.[j] <- 1.0
+                    u.[j] <- 0.0
+                    h.[j] <- boundary t1 xi yj
+                else if j = ny then
+                    l.[j] <- 0.0
+                    d.[j] <- 1.0
+                    h.[j] <- boundary t1 xi yj
+                else
+                    l.[j] <- -Cy
+                    d.[j] <- 2.0 + 2.0*Cy
+                    u.[j] <- -Cy
+                    h.[j] <- 2.0*u0.[i*mstride+j] +
+                             Cx*(u0.[(i-1)*mstride+j] - 2.0*u0.[i*mstride+j] + u0.[(i+1)*mstride+j]) +
+                             dt*(sourceFunction t1 xi yj)
+
+                j <- j + blockDim.x
+
+            __syncthreads()
+
+            triDiagPcrSingleBlock (ny+1) l d u h
+
+            j <- threadIdx.x
+            while j <= ny do 
+                u1.[i*mstride+j] <- h.[j]
+                j <- j + blockDim.x
+
+            __syncthreads()
+
+        i <- i + gridDim.x
+
 /// Exact solution of heat equation 
 ///
 ///     u_t = u_{xx} + u_{yy} + f(t, x, y)
 ///
 /// with boundary condition b(t, x, y) and source function f(t, x, y)
 ///
+/// The x coordinate is mapped to the rows and the y coordinate to the columns
+/// of the solution matrix. The solution matrix is stored in row major format.
 let adiSolver (initCondExpr:Expr<float -> float -> float -> float>) 
                      (boundaryExpr:Expr<float -> float -> float -> float>) 
                      (sourceExpr:Expr<float -> float -> float -> float>) = cuda {
@@ -159,7 +161,6 @@ let adiSolver (initCondExpr:Expr<float -> float -> float -> float>)
             let i = blockIdx.x*blockDim.x + threadIdx.x
             let j = blockIdx.y*blockDim.y + threadIdx.y
             let mstride = ny+1
-            //if i <= nx && j <= ny then u.[i*mstride+j] <- 1.0 @> |> defineKernelFunc
             if i <= nx && j <= ny then u.[i*mstride+j] <- initCond t x.[i] y.[j] @> |> defineKernelFunc
 
     let! xSweepKernel =     
@@ -176,7 +177,6 @@ let adiSolver (initCondExpr:Expr<float -> float -> float -> float>)
 
     return PFunc(fun (m:Module) k tstart tstop Lx Ly nx ny dt  ->
         let maxThreads = m.Worker.Device.Attribute DeviceAttribute.CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK
-        printfn "************ PFunc1"
         let dx = Lx / float(nx)
         let dy = Ly / float(ny)
         let Cx = k * dt / (dx*dx)
@@ -190,23 +190,15 @@ let adiSolver (initCondExpr:Expr<float -> float -> float -> float>)
         use du0 = m.Worker.Malloc<float>(usize)
         use du1 = m.Worker.Malloc<float>(usize)
 
-        //let lp = LaunchParam(dim3(divup (nx+1) 256, divup (ny+1) 256), dim3(256, 256))
         let lp = LaunchParam(dim3(divup (nx+1) 16, divup (ny+1) 16), dim3(16, 16))
         let lpx = LaunchParam(ny+1, nx+1, 4*(nx+1)*sizeof<float>)
         let lpy = LaunchParam(nx+1, ny+1, 4*(ny+1)*sizeof<float>)
-
-        printfn "(%dx%dx%d) (%dx%dx%d)" lp.GridDim.x lp.GridDim.y lp.GridDim.z lp.BlockDim.x lp.BlockDim.y lp.BlockDim.z
-        printfn "nx=%d ny=%d tstart=%f dx.l=%d dy.l=%d du0.l=%d" nx ny tstart dx.Length dy.Length du0.Length
 
         let initCondKernelFunc = initCondKernel.Launch m lp 
         let xSweepKernelFunc = xSweepKernel.Launch m lpx
         let ySweepKernelFunc = ySweepKernel.Launch m lpx
 
-        printfn "setting initial conditions"  
-
         initCondKernelFunc nx ny tstart dx.Ptr dy.Ptr du0.Ptr
-
-        printfn "start looping with tstart = %f tstop = %f" tstart tstop
 
         let mutable t0 = tstart
         let mutable t1 = tstart      
@@ -214,16 +206,12 @@ let adiSolver (initCondExpr:Expr<float -> float -> float -> float>)
             t0 <- t1
             t1 <- t1 + 0.5 * dt
 
-            printfn "xSweep t = %f (dt = %f)" t0 dt
             xSweepKernelFunc nx ny dx.Ptr dy.Ptr Cx Cy dt t0 t1 du0.Ptr du1.Ptr
-            printfn "done xSweep t = %f" t0
 
             t0 <- t1
             t1 <- t1 + 0.5 * dt
 
-            printfn "ySweep t = %f" t0
             ySweepKernelFunc nx ny dx.Ptr dy.Ptr Cx Cy dt t0 t1 du1.Ptr du0.Ptr
-            printfn "done ySweep t = %f" t0
 
         x, y, du0.ToHost()) }
 
--- test/Test.Alea.CUDA.Extension/Heat2dAdi.fs
+++ test/Test.Alea.CUDA.Extension/Heat2dAdi.fs
@@ -13,67 +13,17 @@ let inline maxErr (b:'T[]) (b':'T[]) =
     Array.map2 (fun bi bi' -> abs (bi - bi')) b b' |> Array.max
 
 let [<ReflectedDefinition>] pi = System.Math.PI
-
-[<Test>]
-let ``exp(-t) * sin(pi*x) * cos(pi*y) initial cond`` () =
-
-    let uexact t x y =
-        exp(-t) * sin(pi*x) * cos(pi*y)
-  
-    let initialCondExpr = <@ fun t x y -> exp(-t) * sin(pi*x) * cos(pi*y) @>
-    let boundaryExpr = <@ fun t x y -> exp(-t) * sin(pi*x) * cos(pi*y) @>
-    let sourceFunctionExpr = <@ fun t x y -> exp(-t) * sin(pi*x) * cos(pi*y) * (2.0*pi*pi - 1.0) @>
-
-    printfn "******** 1"
-    let worker = getDefaultWorker()
-    printfn "******** 2"
-    let solve = worker.LoadPModule(adiSolver initialCondExpr boundaryExpr sourceFunctionExpr)
-    printfn "******** 3"
-    let solve = solve.Invoke
-    printfn "******** 4"
-
-    let k = 1.0
-    let tstart = 0.0
-    let tstop = 0.0
-    let Lx = 1.0
-    let Ly = 1.0
-    let dt = 0.01
-
-    let nx = 127
-    let ny = 127
-
-    printfn "******** 5"
-
-    let x, y, u = solve k tstart tstop Lx Ly nx ny dt
-
-    let ue = Array.zeroCreate (x.Length*y.Length)
-    let mstride = ny+1
-    for i = 0 to x.Length-1 do
-        for j = 0 to y.Length-1 do
-            ue.[i*mstride+j] <- uexact tstop x.[i] y.[j]
-            
-    let uErr = maxErr u ue    
-
-    printfn "uErr = %e" uErr
-
     
 [<Test>]
 let ``exp(-t) * sin(pi*x) * cos(pi*y)`` () =
 
-    let uexact t x y =
-        exp(-t) * sin(pi*x) * cos(pi*y)
-  
+    let uexact t x y = exp(-t) * sin(pi*x) * cos(pi*y)
     let initialCondExpr = <@ fun t x y -> exp(-t) * sin(pi*x) * cos(pi*y) @>
     let boundaryExpr = <@ fun t x y -> exp(-t) * sin(pi*x) * cos(pi*y) @>
     let sourceFunctionExpr = <@ fun t x y -> exp(-t) * sin(pi*x) * cos(pi*y) * (2.0*pi*pi - 1.0) @>
 
-    printfn "******** 1"
     let worker = getDefaultWorker()
-    printfn "******** 2"
-    let solve = worker.LoadPModule(adiSolver initialCondExpr boundaryExpr sourceFunctionExpr)
-    printfn "******** 3"
-    let solve = solve.Invoke
-    printfn "******** 4"
+    let solve = worker.LoadPModule(adiSolver initialCondExpr boundaryExpr sourceFunctionExpr).Invoke
 
     let k = 1.0
     let tstart = 0.0
@@ -82,10 +32,8 @@ let ``exp(-t) * sin(pi*x) * cos(pi*y)`` () =
     let Ly = 1.0
     let dt = 0.01
 
-    let nx = 127
-    let ny = 127
-
-    printfn "******** 5"
+    let nx = 16
+    let ny = 16
 
     let x, y, u = solve k tstart tstop Lx Ly nx ny dt
 
@@ -99,5 +47,7 @@ let ``exp(-t) * sin(pi*x) * cos(pi*y)`` () =
 
     printfn "uErr = %e" uErr
 
-    
+    Assert.IsTrue(uErr < 1.2e-4)
 
+
+    
