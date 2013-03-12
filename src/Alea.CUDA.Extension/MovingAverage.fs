596effb849736c653c61cf9e3e3b1bfe3e6416ec
Daniel Egloff <daniel.egloff@quantalea.net>
2013-03-12 19:04:50 +0100

    started with moving average

---
+++ src/Alea.CUDA.Extension/MovingAverage.fs
@@ -0,0 +1,29 @@
+﻿module Alea.CUDA.Extension.MovingAverage
+
+open Microsoft.FSharp.Quotations
+open Alea.Interop.CUDA
+open Alea.CUDA
+
+open Util
+
+let inline normalizedDifferenceKernel () =
+    <@ fun n windowSize (normalizer:'T) (x:DevicePtr<'T>) (y:DevicePtr<'T>) ->
+        let start = blockIdx.x * blockDim.x + threadIdx.x
+        let stride = gridDim.x * blockDim.x
+        let mutable i = start
+        while i < n do
+            y.[i] <- (x.[i + windowSize] - x.[i]) / normalizer
+            i <- i + stride @>
+
+let inline movingAverageTemplate () = cuda {
+
+    let! kernel = normalizedDifferenceKernel () |> defineKernelFunc
+
+    return PFunc(fun (m:Module) windowSize -> 
+        let normalizer:float = NumericLiteralG.FromInt32 windowSize
+
+        ()
+        ) }
+
+
+
---
+++ test/Test.Alea.CUDA.Extension/MovingAverage.fs
@@ -0,0 +1,33 @@
+﻿module Test.Alea.CUDA.Extension.MovingAverage
+
+open System
+open Microsoft.FSharp.Quotations
+open NUnit.Framework
+open Alea.CUDA
+open Alea.CUDA.Extension
+
+/// CPU version to calculate moving average
+let movingAverage n (series:seq<float>) =
+    series    
+    |> Seq.windowed n
+    |> Seq.map Array.sum
+    |> Seq.map (fun a -> a / float n)    
+
+/// Fast CPU version based on arrays to calculate moving average
+let movingAverageFast n (series:array<float>) =
+    let sums = Array.scan (fun s x -> s + x) 0.0 series
+    let ma = Array.zeroCreate (sums.Length - n)
+    for i = n to sums.Length - 1 do
+        ma.[i - n] <- (sums.[i] - sums.[i - n])/float(n)   
+    ma   
+
+let rng = Random(2)
+ 
+let inline maxErr (b:'T[]) (b':'T[]) =
+    Array.map2 (fun bi bi' -> abs (bi - bi')) b b' |> Array.max
+         
+[<Test>]
+let ``moving average`` () =
+
+    ()
+
