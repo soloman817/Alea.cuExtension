0c3d7518f3b5d8029017ca536d1ee09b05ef725e
Xiang Zhang <soloman817@gmail.com>
2013-03-13 14:49:30 +0800

    add timing method for heat guass sample

--- sample/HeatGauss/Program.fs
+++ sample/HeatGauss/Program.fs
@@ -87,6 +87,12 @@ let calc k tstart tstop Lx Ly nx ny dt = pcalc {
     let! u = u.Gather()
     return x, y, u }
 
+type TimingMethod =
+    | None
+    | Timing
+    | TimingLogger
+    | KernelTiming
+
 let heatdist tstop =
     let k = 1.0
     let tstart = 0.0
@@ -98,17 +104,26 @@ let heatdist tstop =
     let nx = 512
     let ny = 512
 
+    let calc = calc k tstart tstop Lx Ly nx ny dt
+    // change this for different timing
+    let tm = TimingMethod.KernelTiming
     let x, y, u =
-        let calc = calc k tstart tstop Lx Ly nx ny dt
-        //calc k tstart tstop Lx Ly nx ny dt |> PCalc.run
-        //let (x, y, u), ktc = calc |> PCalc.runWithKernelTiming 1
-        //let (x, y, u), loggers = calc |> PCalc.runWithTimingLogger
-        let (x, y, u), timings = calc |> PCalc.runWithTiming 3
         printfn "tstop = %f" tstop
-        //ktc.Dump()
-        //loggers.["default"].DumpLogs()
-        printfn "timing = %.6f ms" (timings |> Array.average)
-        x, y, u
+        match tm with
+        | TimingMethod.None ->
+            calc |> PCalc.run
+        | TimingMethod.Timing ->
+            let (x, y, u), timings = calc |> PCalc.runWithTiming 3
+            printfn "timing = %.6f ms" (timings |> Array.average)
+            x, y, u
+        | TimingMethod.KernelTiming ->
+            let (x, y, u), ktc = calc |> PCalc.runWithKernelTiming 1
+            ktc.Dump()
+            x, y, u
+        | TimingMethod.TimingLogger ->
+            let (x, y, u), loggers = calc |> PCalc.runWithTimingLogger
+            loggers.["default"].DumpLogs()
+            x, y, u
 
     x, y, u
 
