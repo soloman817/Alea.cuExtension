71dc06701c6842a96034e237a0806a9bad17987a
Daniel Egloff <daniel.egloff@quantalea.net>
2013-03-15 07:44:27 +0100

    fixed moving average with tests

--- test/Test.Alea.CUDA.Extension/MovingAverage.fs
+++ test/Test.Alea.CUDA.Extension/MovingAverage.fs
@@ -5,6 +5,7 @@ open Microsoft.FSharp.Quotations
 open NUnit.Framework
 open Alea.CUDA
 open Alea.CUDA.Extension
+open Alea.CUDA.Extension.MovingAverage
 
 /// CPU version to calculate moving average
 let movingAverage n (series:seq<float>) =
@@ -14,11 +15,11 @@ let movingAverage n (series:seq<float>) =
     |> Seq.map (fun a -> a / float n)    
 
 /// Fast CPU version based on arrays to calculate moving average
-let movingAverageFast n (series:array<float>) =
+let movingAverageScan windowSize (series:array<float>) =
     let sums = Array.scan (fun s x -> s + x) 0.0 series
-    let ma = Array.zeroCreate (sums.Length - n)
-    for i = n to sums.Length - 1 do
-        ma.[i - n] <- (sums.[i] - sums.[i - n])/float(n)   
+    let ma = Array.zeroCreate (sums.Length - windowSize)
+    for i = windowSize to sums.Length - 1 do
+        ma.[i - windowSize] <- (sums.[i] - sums.[i - windowSize])/float(windowSize)   
     ma   
 
 let rng = Random(2)
@@ -28,6 +29,42 @@ let inline maxErr (b:'T[]) (b':'T[]) =
          
 [<Test>]
 let ``moving average`` () =
+    let worker = getDefaultWorker()
+    let movingAverager = worker.LoadPModule(movingAverager ()).Invoke
+    let test verify eps windowSize (hValues:float[]) = pcalc {
+        // this is a temporary fix until we know how to append a zero in device memory
+        let! dValues = DArray.scatterInBlob worker (Array.append hValues [|0.0|])
+        let! dResult = movingAverager windowSize dValues 
+        let! hResult = dResult.Gather()
 
-    ()
+        if verify then
+            let expected = movingAverageScan windowSize hValues
+            (expected, hResult) ||> Array.iter2 (fun h d -> Assert.That(d, Is.EqualTo(h).Within(eps)))
+    }
+    
+    let values n = let rgn = System.Random(2) in Array.init n (fun i -> rng.NextDouble())
+    let windowSizes = [2; 3; 10]
+    let sizes = [12; 128; 512; 1024; 1200; 4096; 5000; 8191; 8192; 8193; 9000; 10000; 2097152; 8388608]
+    sizes |> Seq.iter (fun n -> windowSizes |> Seq.iter (fun w -> test true 1e-10 w (values n) |> PCalc.run))
 
+[<Test>]
+let ``moving average direct`` () =
+    let worker = getDefaultWorker()
+    let movingAverager = worker.LoadPModule(MovingAvDirect.movingAverager ()).Invoke
+    let test verify eps windowSize (hValues:float[]) = pcalc {
+        let! dValues = DArray.scatterInBlob worker hValues  
+        let! dResult = movingAverager windowSize dValues 
+        let! hResult = dResult.Gather()
+
+        if verify then
+            let expected = movingAverageScan windowSize hValues
+            let hResultCut = Array.sub hResult (windowSize - 1) (hValues.Length - windowSize + 1)
+            (expected, hResultCut) ||> Array.iter2 (fun h d -> Assert.That(d, Is.EqualTo(h).Within(eps)))
+    }
+
+    let values n = let rgn = System.Random(2) in Array.init n (fun i -> rng.NextDouble())
+    let windowSizes = [2; 3; 10]
+    let sizes = [12; 128; 512; 1024; 1200; 4096; 5000; 8191; 8192; 8193]  
+    sizes |> Seq.iter (fun n -> windowSizes |> Seq.iter (fun w -> test true 1e-10 w (values n) |> PCalc.run))
+
+   
